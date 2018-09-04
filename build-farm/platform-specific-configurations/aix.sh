5b6c273cd80540c401f7aa08bee4073890c51d48
John Oliver <johno@insightfullogic.com>
2018-09-04 15:40:49 +0100

    Set AIX java 11

--- build-farm/platform-specific-configurations/aix.sh
+++ build-farm/platform-specific-configurations/aix.sh
@@ -25,7 +25,6 @@ export BUILD_ARGS="${BUILD_ARGS} --skip-freetype"
 
 if [ "${ARCHITECTURE}" == "x64" ] && [ "${VARIANT}" == "openj9" ]
 then
-
   export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} DF=/usr/sysv/bin/df"
 
   if [ "${JAVA_TO_BUILD}" == "${JDK8_VERSION}" ]
@@ -37,5 +36,47 @@ then
   elif [ "${JAVA_TO_BUILD}" == "${JDK10_VERSION}" ]
   then
     export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-freemarker-jar=/ramdisk0/build/workspace/openjdk10_openj9_build_ppc64_aix/freemarker-2.3.8/lib/freemarker.jar"
+  elif [ "${JAVA_TO_BUILD}" == "${JDK11_VERSION}" ]
+  then
+    export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-freemarker-jar=/ramdisk0/build/workspace/openjdk10_openj9_build_ppc64_aix/freemarker-2.3.8/lib/freemarker.jar DF=/usr/sysv/bin/df"
   fi
 fi
+
+if
+
+if [ "${JAVA_TO_BUILD}" == "${JDK11_VERSION}" ]
+then
+  export JDK10_BOOT_DIR="$PWD/jdk-10"
+  if [ ! -d "$JDK10_BOOT_DIR/bin" ]; then
+    mkdir -p "$JDK10_BOOT_DIR"
+    wget -q -O - 'https://api.adoptopenjdk.net/v2/binary/releases/openjdk10?os=aix&release=latest' | tar xpzf - --strip-components=2 -C "$JDK10_BOOT_DIR"
+  fi
+
+  if [ "${VARIANT}" == "hotspot" ]; then
+    export CONFIGURE_ARGS_FOR_ANY_PLATFORM="${CONFIGURE_ARGS_FOR_ANY_PLATFORM} --with-freemarker-jar=/ramdisk0/build/workspace/openjdk10_openj9_build_ppc64_aix/freemarker-2.3.8/lib/freemarker.jar DF=/usr/sysv/bin/df"
+  fi
+
+  export JDK_BOOT_DIR=$JDK10_BOOT_DIR
+
+  export LANG=C
+  export PATH=/opt/freeware/bin:$JAVA_HOME/bin:/usr/local/bin:/opt/IBM/xlC/13.1.3/bin:/opt/IBM/xlc/13.1.3/bin:$PATH
+  export CONFIGURE_ARGS_FOR_ANY_PLATFORM="--disable-warnings-as-errors ${CONFIGURE_ARGS_FOR_ANY_PLATFORM}"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
