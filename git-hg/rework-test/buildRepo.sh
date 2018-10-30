33e8ae17af4b736976abaad780410a4053a802f4
John Oliver <johno@insightfullogic.com>
2018-10-30 18:26:16 +0000

    Fix master

--- git-hg/rework-test/buildRepo.sh
+++ git-hg/rework-test/buildRepo.sh
@@ -13,8 +13,6 @@ REPO="$WORKSPACE/test-repo/"
 PATCHES="$SCRIPT_DIR/patches/"
 mkdir -p "$REPO"
 
-
-
 cd "$REPO"
 
 if [ -d ".git" ];then
@@ -28,21 +26,40 @@ cd "$SCRIPT_DIR"
 
 chmod +x merge.sh
 
-# Init new repo to head
+# update mirrors
 ./merge.sh -u
+
+
+################################################
+## Build master
+## Init master to be synced at jdk8u181-b13
 ./merge.sh -r -s "jdk8u181-b13"
+################################################
+
 
 
 ################################################
 ## Build dev
-## dev is HEAD track with our patches
-
+## dev branch is HEAD track with our patches
 cd "$REPO"
 git checkout -b dev
+
+# Apply our patches
 git am $PATCHES/company_name.patch
+
+# Update dev to HEAD
 cd $SCRIPT_DIR
 ./merge.sh -s "jdk8u181-b13" -e "HEAD" -b "dev"
+################################################
+
+
+
+################################################
+## Push master up to head
+cd "$REPO"
 git checkout master
+
+cd $SCRIPT_DIR
 ./merge.sh -s "jdk8u181-b13" -e "HEAD" -b "master"
 ################################################
 
@@ -73,30 +90,5 @@ git branch "jdk8u181-b13"
 cd $SCRIPT_DIR
 ./merge.sh -s "jdk8u181-b13" -e "jdk8u192-b12" -b "release"
 ################################################
-
-
-
-#./merge.sh -u -s "jdk8u144-b34"
-#./merge.sh -r -s "jdk8u144-b34"
-#./merge.sh -s "jdk8u144-b34" -e "jdk8u162-b12"
-#./merge.sh -s "jdk8u162-b12" -e "jdk8u172-b11"
-#./merge.sh -s "jdk8u172-b11" -e "jdk8u181-b13"
-
-#cd $REPO
-#git checkout master
-#git log
-#git am  $PATCHES/1.patch
-#git am  $PATCHES/2.patch
-#git am  $PATCHES/3.patch
-
-#cd $SCRIPT_DIR
-
-#./merge.sh -s "jdk8u181-b13" -e "jdk8u192-b12"
-
-#./merge.sh -s "jdk8u192-b12" -e "HEAD"
-
-#cd $REPO
-#git push git@github.com:AdoptOpenJDK/openjdk-jdk8u-test.git
-#git push git@github.com:AdoptOpenJDK/openjdk-jdk8u-test.git --tags
 
 
