92ea85013ac372407bb3d7d30656c83612e1ffbe
John Oliver <1615532+johnoliver@users.noreply.github.com>
2019-01-23 16:38:47 +0000

    Initial test

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -34,6 +34,57 @@ import groovy.json.JsonSlurper
  *
  */
 
+
+
+def getJavaVersionNumber(version) {
+    // version should be something like "jdk8u"
+    def matcher = (version =~ /(\d+)/)
+    return Integer.parseInt(matcher[0][1])
+}
+
+def parseVersion(version) {
+    //Regexes based on those in http://openjdk.java.net/jeps/223
+    // Technically the standard supports an arbitrary number of numbers, we will support 3 for now
+    final vnumRegex = "(?<major>[0-9]+)(\\.(?<minor>[0-9]+))?(\\.(?<security>[0-9]+))?";
+    final pre = "(?<pre>[a-zA-Z0-9]+)";
+    final build = "(?<build>[0-9]+)";
+    final opt = "(?<opt>[-a-zA-Z0-9\\.]+)";
+
+    final version223Regexs = [
+            "(?<version>${vnumRegex}(\\-${pre})?\\+${build}(\\-${opt})?)",
+            "(?<version>${vnumRegex}\\-${pre}(\\-${opt})?)",
+            "(?<version>${vnumRegex}(\\+\\-${opt})?)"
+    ];
+
+    final pre223regex = "jdk(?<version>(?<major>[0-8]+)(u(?<update>[0-9]+))?(-b(?<build>[0-9]+))(_(?<opt>[-a-zA-Z0-9\\.]+))?)";
+    final matched = version =~ /${pre223regex}/
+
+    echo "matching: " + version
+    if (matched.matches()) {
+        return [
+                major   : matched.group('major'),
+                minor   : matched.group('minor'),
+                security: matched.group('security'),
+                pre     : matched.group('pre'),
+                build   : matched.group('build'),
+                opt     : matched.group('opt'),
+                version : matched.group('version')
+
+        ]
+    } else {
+        /*
+        version223Regexs
+                .map { regex ->
+            final matched = version =~ /${pre223regex}/
+            if (matched.matches()) {
+
+            }
+        }*/
+        return [];
+    }
+}
+
+
 def determineTestJobName(config, testType) {
 
     def variant
@@ -184,11 +235,8 @@ try {
 
     node("master") {
 
-        echo "loading"
-
-        final Versions = load "pipelines/build/common/versions.groovy"
         echo "calling"
-        def versionData = Versions.parseVersion(config.parameters.TAG)
+        def versionData = parseVersion(config.parameters.TAG)
         echo "printing"
         echo JsonOutput.prettyPrint(JsonOutput.toJson(versionData))
         return
